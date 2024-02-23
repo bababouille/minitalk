21bbe4d06486617cadce3dcf2e98da2cffce7bf5
bababouille <cambier.vo@gmail.com>
2024-02-23 22:46:14 +0000

    minitalk with bonus

--- client.c
+++ client.c
@@ -4,6 +4,32 @@
 #include <stdlib.h>
 #include <signal.h>
 
+#define DEBUG 0  // Set this to 1 to enable debugging, 0 to disable
+
+#define debug_printf(fmt, ...) \
+            do { if (DEBUG) fprintf(stderr, "[%s:%d] " fmt, __FILE__, __LINE__, ##__VA_ARGS__); } while (0)
+void handle_that_bitch(int signal)
+{
+	static int bit;
+	static int storage;
+
+	if(signal == SIGUSR1)//only enter if 1 is found in client
+	{
+		storage |= (1 << bit);
+	}
+	bit++;
+	if(bit == 8)
+	{
+		if(storage == 0)
+		{
+			exit(0);
+		}
+		printf("%c", storage);
+		bit = 0;
+		storage = 0;
+	}
+
+}
 void	sender(int pid, char c)
 {
 	int bit;
@@ -36,13 +62,60 @@ void	sender(int pid, char c)
 	}
 }
 
+void	senderpid(int pid, pid_t my_pid)
+{
+	int bit;
+	bit = 0;
+	// x = 	00000000
+	// c = 	01010101
+	// check c for 1 and send sigusr1 if correct
+	// doit bit by bit
+	
+	while(bit < (sizeof(my_pid)* 8))
+	{
+		if ((my_pid & (1 << bit)) != 0)
+		{
+			debug_printf("plus de detail 1\n");
+			if (kill(pid, SIGUSR1) == -1)
+			{
+				perror("Error sending SIGUSR1");
+                exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			debug_printf("plus de detail aussi0\n");
+			if (kill(pid, SIGUSR2) == -1)
+			{
+				perror("Error sending SIGUSR1");
+                exit(EXIT_FAILURE);
+			}
+		}
+		bit++;
+		usleep(100);
+	}
+}
+
 int main(int argc, char **argv)
 {
 	int pid;
 	int x = 0;
+	pid_t my_pid;
+	my_pid = getpid();
+	debug_printf("mon pid %u\n",my_pid);
 	
 	if (argc == 3)
 	{
+		if(signal(SIGUSR1, handle_that_bitch) == SIG_ERR)
+		{
+			perror("Error setting up signal handler for SIGUSR1");
+        	exit(EXIT_FAILURE);
+		}
+		if(signal(SIGUSR2, handle_that_bitch) == SIG_ERR)
+		{
+			perror("Error setting up signal handler for SIGUSR1");
+        	exit(EXIT_FAILURE);
+		}
 		pid = atoi(argv[1]);
 		while(argv[2][x] != '\0')
 		{
@@ -50,6 +123,13 @@ int main(int argc, char **argv)
 			x++;
 		}
 		sender(pid, '\n');
+		sender(pid, 0);
+		senderpid(pid, my_pid);
+
+		while (1)
+		{
+			pause();
+		}
 	}
 	else
 	{
--- server.c
+++ server.c
@@ -15,23 +15,79 @@
 #include <stdlib.h>
 #include <signal.h>
 
+void	sender(int pid, char c)
+{
+	int bit;
+	bit = 0;
+	// x = 	00000000
+	// c = 	01010101
+	// check c for 1 and send sigusr1 if correct
+	// doit bit by bit
+	
+	while(bit < 8)
+	{
+		if ((c & (1 << bit)) != 0)
+		{
+			if (kill(pid, SIGUSR1) == -1)
+			{
+				perror("Error sending SIGUSR1");
+                exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+			{
+				perror("Error sending SIGUSR1");
+                exit(EXIT_FAILURE);
+			}
+		}
+		bit++;
+		usleep(100);
+	}
+}
+
 void	handle_that_bitch(int signal)
 {
-	// when receiving a Signal transform the bit passed frol client
-	// make an int of value o and pass the bit value into it
+	// when receiving a Signal transform the bit passed from client
+	// make an int of value 0 and pass the bit value into it
 	static int bit;
 	static int storage;
-	if(signal == SIGUSR1)//olny enter if one if found in client
+	static int x;
+
+
+	if(signal == SIGUSR1)//only enter if 1 is found in client
 	{
 		storage |= (1 << bit);
 	}
 	bit++;
-	if(bit == 8)
+	if(bit == 8 && x == 0)
 	{
+		if (storage == 0)
+		{
+			x = 1;
+		}
 		printf("%c", storage);
 		bit = 0;
-		storage = 0;//not neede its redudant but its good practice, wtf
+		storage = 0;
 	}
+	if (bit == (sizeof(pid_t) * 8) && x == 1)//pid_t = 4 byte so 32 bit( sizeof because i am a cool boy)
+	{
+		//printf("%u\n", storage);
+		char nikita[] = "message accepted you motherfucker";
+		int n = 0;
+		while(nikita[n])
+		{
+			sender(storage, nikita[n]);
+			n++;
+		}
+		sender(storage, '\n');
+		sender(storage, 0);
+		x = 0;
+		bit = 0;
+		storage = 0;
+	}
+	
 }
 int main(int argc, char**argv)
 {
@@ -44,11 +100,8 @@ int main(int argc, char**argv)
 		return 0;
 	}
 	printf("My beautifull pid is: %d\n", pid);
-	printf("Pleas
+	printf("Please wait for signal motherfucker\n");
 	
-	e wait for signal motherfucker\n");
-	while (argc == 1)
-	{
 		//signal() returns a function pointer (SIG_ERR or a pointer to the previous signal handler)
 		if(signal(SIGUSR1, handle_that_bitch) == SIG_ERR)
 		{
@@ -60,8 +113,9 @@ int main(int argc, char**argv)
 			perror("Error setting up signal handler for SIGUSR1");
         	exit(EXIT_FAILURE);
 		}
+	while (argc == 1)
+	{	
 		pause();
 	}
-	
 	return 0;
 }
